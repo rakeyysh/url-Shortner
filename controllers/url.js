c2ac8a475fb47b09db588bb5fb5a89ab342370c0
rakeyysh <8349733408a@gmail.com>
2025-07-31 01:03:56 +0530

    EJS intro

--- controllers/url.js
+++ controllers/url.js
@@ -4,17 +4,24 @@ const URL  = require('../models/url');
 async function handleGenerateNewShortURL(req,res){
 
     const body = req.body;
-    console.log(body);
+    console.log("BODY",body);
     if(!body.url) return res.status(400).json({error: 'url is required'})
     const shortID = nanoid(8);
-    await URL.create({
+    const newURL = await URL.create({
         shortID: shortID,
         redirectURL: body.url,
         visitHistory: [],
 
     })
-    return res.json({id:shortID});
+    console.log("Created:",newURL);
+    return res.render("home",{
+        id:shortID,
+    })
+
+
+    
 }
+
 
 
 async function handleGetAnalytics(req,res){
--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const express = require("express");
 const { connectToMongoDB } = require('./connect');
 const URL = require('./models/url');
+const path = require("path");
+const staticRoute = require("./routes/staticRouter");
+const mongoose = require("mongoose");
+
 
 
 const urlRoute = require('./routes/url')
 const app = express();
 const PORT = 8001;
 
+
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use('/url',urlRoute);
+app.use("/",staticRoute);
+
+
 
 
 connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(()=>{
-    console.log("MongoDB connected");
+     console.log("MongoDB connected");
+        
 })
-app.use('/url',urlRoute);
+
+
+
+
+
 // console.log(req);
-app.get('/:shortID',async(req,res)=>{
+app.get('/url/:shortID',async(req,res)=>{
     const shortID = req.params.shortID;
   const entry =  await URL.findOneAndUpdate({
         shortID,
@@ -26,6 +45,7 @@ app.get('/:shortID',async(req,res)=>{
                 timestamp: Date.now()},
         }
     })
+    console.log(entry);
     res.redirect(entry.redirectURL);
 })
 
